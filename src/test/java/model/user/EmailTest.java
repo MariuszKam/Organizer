ca0adad3ce5c98ad3a5029d33ea7591050f46d0a
MariuszKam <rexxarhell@gmail.com>
2025-08-09 22:42:54 +0200

    Updated test for User, and Email

--- src/test/java/model/user/EmailTest.java
+++ src/test/java/model/user/EmailTest.java
@@ -11,7 +11,7 @@ public class EmailTest {
 
     @Test
     public void testEmailWhenNull() {
-        assertThrows(IllegalArgumentException.class, () ->
+        assertThrows(NullPointerException.class, () ->
                 new Email(null));
     }
 
@@ -30,7 +30,7 @@ public class EmailTest {
     @Test
     public void testEmailWhenValid() {
         Email validEmail = new Email(emailAddress);
-        assertEquals(emailAddress, validEmail.getEmailAddress(), String.format("Expected email address to be '%s'", emailAddress));
+        assertEquals(emailAddress, validEmail.emailAddress(), String.format("Expected email address to be '%s'", emailAddress));
     }
 
     @Test
@@ -38,8 +38,8 @@ public class EmailTest {
         Email email1 = new Email(emailAddress);
         Email email2 = new Email(emailAddress);
         assertEquals(email1, email2,
-                     String.format("Expected emails to be equal: '%s' and '%s'", email1.getEmailAddress(),
-                                   email2.getEmailAddress()));
+                     String.format("Expected emails to be equal: '%s' and '%s'", email1.emailAddress(),
+                                   email2.emailAddress()));
     }
 
     @Test
@@ -47,8 +47,8 @@ public class EmailTest {
         Email email1 = new Email(emailAddress);
         Email email2 = new Email("example2@org.com");
         assertNotEquals(email1, email2,
-                        String.format("Expected emails to be different: '%s' and '%s'", email1.getEmailAddress(),
-                                      email2.getEmailAddress()));
+                        String.format("Expected emails to be different: '%s' and '%s'", email1.emailAddress(),
+                                      email2.emailAddress()));
     }
 
     @Test
@@ -56,8 +56,8 @@ public class EmailTest {
         Email email1 = new Email(emailAddress);
         Email email2 = new Email(emailAddress);
         assertEquals(email1.hashCode(), email2.hashCode(),
-                     String.format("Expected hash codes to be equal for emails: '%s' and '%s", email1.getEmailAddress(),
-                                   email2.getEmailAddress()));
+                     String.format("Expected hash codes to be equal for emails: '%s' and '%s", email1.emailAddress(),
+                                   email2.emailAddress()));
     }
 
     @Test
@@ -66,7 +66,30 @@ public class EmailTest {
         Email email2 = new Email("example2@org.com");
         assertNotEquals(email1.hashCode(), email2.hashCode(),
                         String.format("Expected hash codes to be different for emails: '%s' and '%s'",
-                                      email1.getEmailAddress(),
-                                      email2.getEmailAddress()));
+                                      email1.emailAddress(),
+                                      email2.emailAddress()));
+    }
+
+    @Test
+    public void testUpperLowerEquality() {
+        Email email = Email.of("ABC@ORG.COM");
+        assertEquals("abc@org.com", email.emailAddress(),
+                String.format("Expected email address to be lowercased: '%s'", email.emailAddress()));
+    }
+
+    @Test
+    public void testInCorrectFormat() {
+        assertThrows(IllegalArgumentException.class, () ->
+                new Email("invalid-email@.com"));
+    }
+
+    @Test
+    public void testMaskingEmail() {
+        Email email = new Email(emailAddress);
+        String maskedEmail = email.toString();
+        assertTrue(maskedEmail.startsWith("e***@"),
+                String.format("Expected masked email to start with 'e***@', but was '%s'", maskedEmail));
+        assertTrue(maskedEmail.endsWith("@org.com"),
+                String.format("Expected masked email to end with '@org.com', but was '%s'", maskedEmail));
     }
 }
