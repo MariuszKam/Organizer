c530fc0118127019c76f23baabbe4b3ed65e4c8c
MariuszKam <rexxarhell@gmail.com>
2025-08-10 01:26:08 +0200

    Fixed name() to name

--- src/main/java/com/organizer/model/task/TaskName.java
+++ src/main/java/com/organizer/model/task/TaskName.java
@@ -8,8 +8,8 @@ public record TaskName(String name) {
     private static final Pattern PATTERN = Pattern.compile("^[a-zA-Z0-9_\\- ]{1,50}$");
 
     public TaskName {
-        Objects.requireNonNull(name(), "Task name cannot be null");
-        var taskName = name().strip();
+        Objects.requireNonNull(name, "Task name cannot be null");
+        var taskName = name.strip();
         if (!PATTERN.matcher(name).matches()) throw new IllegalArgumentException(String.format("Invalid task name: %s", taskName));
         name = taskName;
     }
