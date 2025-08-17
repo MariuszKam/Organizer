8e1c20e4e5441f72be6867791710ed28317d2889
MariuszKam <rexxarhell@gmail.com>
2025-08-17 18:13:27 +0200

    Package UserIdTest.java

--- src/test/java/model/user/UserIdTest.java
+++ src/test/java/model/user/UserIdTest.java
@@ -10,7 +10,7 @@ import java.util.UUID;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("UserId Value Object Tests")
-public class UserIdTest {
+class UserIdTest {
 
     @Nested
     @DisplayName("Creation")
