0bd184ad220313f1545da8b96d0969e8b2098f6d
MariuszKam <rexxarhell@gmail.com>
2025-08-23 16:37:35 +0200

    Refactored CreateUserServiceTest

--- src/test/java/service/user/usecase/create/CreateUserServiceTest.java
+++ src/test/java/service/user/usecase/create/CreateUserServiceTest.java
@@ -2,7 +2,6 @@ package service.user.usecase.create;
 
 import com.organizer.service.user.adapter.InMemoryUserStore;
 import com.organizer.service.user.adapter.UUIDGenerator;
-import com.organizer.service.user.port.IdGenerator;
 import com.organizer.service.user.port.UserStore;
 import com.organizer.service.user.usecase.create.CreateUserCommand;
 import com.organizer.service.user.usecase.create.CreateUserResult;
@@ -20,13 +19,13 @@ class CreateUserServiceTest {
     private static final String VALID_USERNAME = "validUser";
     private static final String VALID_EMAIL = "example@org.com";
 
+    private CreateUserService service;
     private UserStore userStore;
-    private IdGenerator idGenerator;
 
     @BeforeEach
     void setUp() {
         userStore = new InMemoryUserStore();
-        idGenerator = new UUIDGenerator();
+        service = new CreateUserService(userStore, new UUIDGenerator());
     }
 
     @Nested
@@ -40,9 +39,7 @@ class CreateUserServiceTest {
             @Test
             @DisplayName("should return MISSING_COMMAND when command is null")
             void shouldReturnMissingCommandWhenCommandIsNull() {
-                CreateUserCommand command = null;
-                CreateUserService service = new CreateUserService(userStore, idGenerator);
-                CreateUserResult result = service.handle(command);
+                CreateUserResult result = service.handle(null);
                 assertEquals(CreateUserResult.Error.MISSING_COMMAND, result,
                         String.format("Expected MISSING_COMMAND but got: %s", result));
             }
@@ -58,7 +55,6 @@ class CreateUserServiceTest {
             void shouldReturnUsernameAlreadyExistsWhenUsernameExists() {
                 CreateUserCommand command1 = new CreateUserCommand(VALID_USERNAME, VALID_EMAIL);
                 CreateUserCommand command2 = new CreateUserCommand(VALID_USERNAME, "example2@org.com");
-                CreateUserService service = new CreateUserService(userStore, idGenerator);
                 service.handle(command1);
                 CreateUserResult result2 = service.handle(command2);
                 assertEquals(CreateUserResult.Error.USERNAME_ALREADY_EXISTS, result2,
@@ -70,7 +66,6 @@ class CreateUserServiceTest {
             @DisplayName("should return INVALID_USERNAME_FORMAT when username format is wrong")
             void shouldReturnInvalidUsernameWhenFormatIsWrong() {
                 CreateUserCommand command = new CreateUserCommand("ab", VALID_EMAIL);
-                CreateUserService service = new CreateUserService(userStore, idGenerator);
                 CreateUserResult result = service.handle(command);
                 assertEquals(CreateUserResult.Error.INVALID_USERNAME_FORMAT, result,
                         String.format("Expected INVALID_USERNAME_FORMAT but got: %s", result));
@@ -80,7 +75,6 @@ class CreateUserServiceTest {
             @DisplayName("should return MISSING_USERNAME when username is null")
             void shouldReturnMissingUsernameWhenUsernameIsNull() {
                 CreateUserCommand command = new CreateUserCommand(null, VALID_EMAIL);
-                CreateUserService service = new CreateUserService(userStore, idGenerator);
                 CreateUserResult result = service.handle(command);
                 assertEquals(CreateUserResult.Error.MISSING_USERNAME, result,
                         String.format("Expected MISSING_USERNAME but got: %s", result));
@@ -97,7 +91,6 @@ class CreateUserServiceTest {
             void shouldReturnEmailAlreadyExistsWhenEmailExists() {
                 CreateUserCommand command1 = new CreateUserCommand(VALID_USERNAME, VALID_EMAIL);
                 CreateUserCommand command2 = new CreateUserCommand("anotherUser", VALID_EMAIL);
-                CreateUserService service = new CreateUserService(userStore, idGenerator);
                 service.handle(command1);
                 CreateUserResult result2 = service.handle(command2);
                 assertEquals(CreateUserResult.Error.EMAIL_ALREADY_EXISTS, result2,
@@ -108,7 +101,6 @@ class CreateUserServiceTest {
             @DisplayName("should return MISSING_EMAIL when email is null")
             void shouldReturnMissingEmailWhenEmailIsNull() {
                 CreateUserCommand command = new CreateUserCommand(VALID_USERNAME, null);
-                CreateUserService service = new CreateUserService(userStore, idGenerator);
                 CreateUserResult result = service.handle(command);
                 assertEquals(CreateUserResult.Error.MISSING_EMAIL, result,
                         String.format("Expected MISSING_EMAIL but got: %s", result));
@@ -118,7 +110,6 @@ class CreateUserServiceTest {
             @DisplayName("should return INVALID_EMAIL_FORMAT when email format is wrong")
             void shouldReturnInvalidEmailWhenFormatIsWrong() {
                 CreateUserCommand command = new CreateUserCommand(VALID_USERNAME, "invalid-email");
-                CreateUserService service = new CreateUserService(userStore, idGenerator);
                 CreateUserResult result = service.handle(command);
                 assertEquals(CreateUserResult.Error.INVALID_EMAIL_FORMAT, result,
                         String.format("Expected INVALID_EMAIL_FORMAT but got: %s", result));
@@ -136,7 +127,6 @@ class CreateUserServiceTest {
         @DisplayName("should create user successfully with valid command")
         void shouldCreateUserSuccessfullyWithValidCommand() {
             CreateUserCommand command = new CreateUserCommand(VALID_USERNAME, VALID_EMAIL);
-            CreateUserService service = new CreateUserService(userStore, idGenerator);
             CreateUserResult result = service.handle(command);
             assertInstanceOf(CreateUserResult.Ok.class, result,
                     String.format("Expected result to be instance of Ok but got: %s", result));
@@ -146,7 +136,6 @@ class CreateUserServiceTest {
         @DisplayName("should match generated userId in result")
         void shouldMatchGeneratedUserIdInResult() {
             CreateUserCommand command = new CreateUserCommand(VALID_USERNAME, VALID_EMAIL);
-            CreateUserService service = new CreateUserService(userStore, idGenerator);
             CreateUserResult result = service.handle(command);
             assertInstanceOf(CreateUserResult.Ok.class, result,
                     String.format("Expected result to be instance of Ok but got: %s", result));
